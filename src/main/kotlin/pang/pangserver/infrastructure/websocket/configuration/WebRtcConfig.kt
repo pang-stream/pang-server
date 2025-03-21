13eefa36cbaf75b98af5e4c1ee5326a82814d4a0
kmg <legolove08@naver.com>
2025-03-21 09:50:05 +0900

    feat: config

---
+++ src/main/kotlin/pang/pangserver/infrastructure/websocket/configuration/WebRtcConfig.kt
@@ -0,0 +1,10 @@
+package pang.pangserver.infrastructure.websocket.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+
+@Configuration
+@EnableWebSocket
+class WebRtcConfig {
+
+}
