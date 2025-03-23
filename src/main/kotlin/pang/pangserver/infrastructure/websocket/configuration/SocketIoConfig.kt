74a46fa5d10dd33b321ff1bceb0cabc3e117da93
kmg <legolove08@naver.com>
2025-03-23 23:45:12 +0900

    faet: socket io

---
+++ src/main/kotlin/pang/pangserver/infrastructure/websocket/configuration/SocketIoConfig.kt
@@ -0,0 +1,24 @@
+package pang.pangserver.infrastructure.websocket.configuration
+
+import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.Configuration as SocketIoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import pang.pangserver.infrastructure.websocket.properties.SocketIoProperties
+
+@Configuration
+@EnableWebSocket
+@EnableConfigurationProperties(SocketIoProperties::class)
+class SocketIoConfig(
+    private val properties: SocketIoProperties,
+) {
+    @Bean
+    fun socketIoServer(): SocketIOServer {
+        val config = SocketIoConfiguration()
+        config.hostname = properties.host
+        config.port = properties.prot
+        return SocketIOServer(config)
+    }
+}
---
+++ src/main/kotlin/pang/pangserver/infrastructure/websocket/properties/SocketIoProperties.kt
@@ -0,0 +1,9 @@
+package pang.pangserver.infrastructure.websocket.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "socket")
+data class SocketIoProperties(
+    val host: String,
+    val prot: Int
+)
