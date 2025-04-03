e16bd57fd22c2b10b5fd426c3473bcac98f846d6
kmg <legolove08@naver.com>
2025-04-03 16:40:28 +0900

    chore: logic

--- src/main/kotlin/pang/pangserver/infrastructure/security/filter/TokenFilter.kt
+++ src/main/kotlin/pang/pangserver/infrastructure/security/filter/TokenFilter.kt
@@ -31,7 +31,7 @@ class TokenFilter (
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        if (request.getHeader(HttpHeaders.AUTHORIZATION).isNotEmpty()) {
+        if (!request.getHeader(HttpHeaders.AUTHORIZATION).isNullOrEmpty()) {
             val token: String = request.getHeader("Authorization")?: throw EmptyTokenException()
             if (!token.startsWith(TOKEN_SECURE_TYPE)) throw EmptyTokenException()
             tokenValidator.validateAll(token.removePrefix(TOKEN_SECURE_TYPE), TokenType.ACCESS_TOKEN)
